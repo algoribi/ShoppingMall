6e8285b8c6257633e9a12a98194a5b7a32ad9e76
algoribi <algoribi36@gmail.com>
2022-03-16 01:09:07 +0900

    update

--- ShoppingMallFront/src/components/printProductPage.tsx
+++ ShoppingMallFront/src/components/printProductPage.tsx
@@ -3,16 +3,17 @@ import products from '../db/products.json';
 export default function PrintProductPage() {
 
     const productClick = () => {
-
+        // css 마우스 대면 박스 테두리 보이는 이벤트
+        // 클릭하면 alert -> 장바구니에 담으시겠습니까?
     }
 
     return (
         <div className='products'>
             {products.map(product => (
-                // todo : 상품 이미지 추가 + 상품 목록 보이기 css
                 <div className='product_box' key={product.productCode} onClick={productClick}>
-                    <img src={`../db/img/${product.productCode}.jpg`} alt='product' ></img>
-                    {product.name} ({product.price})
+                    <img className='product_img' src={`/images/${product.productCode}.jpg`} />
+                    <p className='product_name'>{product.name}</p>
+                    <p className='product_price'>{product.price.toLocaleString()} 원</p>
                 </div>
             ))}
         </div>
