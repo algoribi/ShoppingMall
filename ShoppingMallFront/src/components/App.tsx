97e7e89e0d66867cd058c0209d54d645db31b0aa
algoribi <algoribi36@gmail.com>
2022-03-17 01:21:40 +0900

    update

--- ShoppingMallFront/src/components/App.tsx
+++ ShoppingMallFront/src/components/App.tsx
@@ -5,52 +5,22 @@ import PrintCartPage from './printCartPage';
 import PrintProductPage from './printProductPage';
 import { ProductType } from 'types';
 
-class Cart {
-
-}
-
 export default function App() {
     const [cartPage, setCartPage] = useState(false);
-    const [cartList, setCartList] = useState<ProductType[]>([]);
+    const cart = new Cart();
 
     const toggleCartButton = () => {
         setCartPage(cartPage ? false : true);
     }
 
-    const addProduct = (product : ProductType) => {
-        alert(`'${product.name}'를 장바구니에 추가하시겠습니까?`);
-
-        let checkSameProduct : boolean = false;
-
-        let newCartList = cartList.map(item => {
-            let counter : number = item.productCount;
-            if (item.productCode === product.productCode) {
-                checkSameProduct = true;
-                counter = item.productCount + 1;
-            }
-            return ({
-                productCode : item.productCode,
-                name : item.name,
-                price : item.price,
-                productCount : counter
-            })
-        });
-
-        if (!checkSameProduct) {
-            newCartList = [...newCartList, {
-                productCode : product.productCode,
-                name : product.name,
-                price : product.price,
-                productCount : product.productCount
-            }];
-        }
-
-        setCartList(newCartList);
+    const addCartProduct = (product : ProductType) => {
+        cart.addProduct(product);
+        console.log(cart.cartList);
     }
 
     const deleteCartProduct = (productCode : string, name : string) => {
-        const newCartList = cartList.filter(item => item.productCode !== productCode);
-        setCartList(newCartList);
+        // const newCartList = cartList.filter(item => item.productCode !== productCode);
+        // setCartList(newCartList);
 
         alert(`'${name}'를 장바구니에서 삭제했습니다.`);
     }
@@ -62,13 +32,26 @@ export default function App() {
             <Header />
         </div>
         <div className='button_box'>
-            <button onClick={toggleCartButton} className={cartPage ? 'home_button' : 'cart_button' }></button>
+            <button onClick={toggleCartButton} className={cartPage ? 'home_button button' : 'cart_button button' }></button>
         </div>
         {
             cartPage
-                ? <PrintCartPage cartList={cartList} deleteCartProduct={deleteCartProduct} />
-                : <PrintProductPage cartList={cartList} addProduct={addProduct} />
+                ? <PrintCartPage cartList={cart.cartList} deleteCartProduct={deleteCartProduct} />
+                : <PrintProductPage cartList={cart.cartList} addCartProduct={addCartProduct} />
         }
     </div>
     );
+}
+
+class Cart {
+    readonly cartList: ProductType[] = [];
+    
+    addProduct(product : ProductType) {
+        this.cartList.push(product);
+    }
+
+    deleteProduct() {
+
+    }
+
 }
--- ShoppingMallFront/src/components/printProductPage.tsx
+++ ShoppingMallFront/src/components/printProductPage.tsx
@@ -1,32 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import products from '../db/products.json';
 import { ProductType } from 'types';
 
 interface Props {
     cartList : ProductType[];
-    addProduct : (value : ProductType) => void;
+    addCartProduct : (value : ProductType) => void;
 }
 
-export default function PrintProductPage(props : Props) {
-
+export default function PrintProductPage(props: Props) {
+    const [toggleModal, setToggleModal] = useState(false);
+    const [saveProduct, setSaveProduct] = useState({
+        productCode: "",
+        name: "",
+        price: 0,
+        productCount: 0
+    });
+    
     return (
-        <>
-        <p className='page_title'>상품목록</p>
-        <div className='products'>
-            {products.map(product => (
-                <div className='product_box' key={product.productCode}
-                    onClick={() => props.addProduct({
-                        productCode : product.productCode,
-                        name : product.name,
-                        price : product.price,
-                        productCount : 1
-                    })}>
-                    <img className='product_img' src={`/images/${product.productCode}.jpg`} alt={`${product.productCode}_img`} />
-                    <p className='product_name'>{product.name}</p>
-                    <p className='product_price'>{product.price.toLocaleString()} 원</p>
-                </div>
-            ))}
+        <div>
+            <p className='page_title'>상품목록</p>
+            <div className='products'>
+                {products.map(product => (
+                    <div className='product_box' key={product.productCode}
+                        onClick={() => { setSaveProduct({
+                            productCode : product.productCode,
+                            name : product.name,
+                            price : product.price,
+                            productCount : 1
+                        }); setToggleModal(true);} }>
+                        <img className='product_img' src={`/images/${product.productCode}.jpg`} alt={`${product.productCode}_img`} />
+                        <p className='product_name'>{product.name}</p>
+                        <p className='product_price'>{product.price.toLocaleString()} 원</p>
+                    </div>
+                ))}
+            </div>
+            <div className={ toggleModal ? 'modal_box' : 'non_box' }>
+                <div className='modal_message_box'>
+                    <p className='modal_text'>장바구니에 추가할까요?</p>
+                    <button className='modal_button button' onClick={() => {props.addCartProduct(saveProduct); setToggleModal(false);}}>
+                        YES
+                    </button>
+                    <button className='modal_button button' onClick={() => setToggleModal(false)}>NO</button>
+                </div>    
+            </div>
         </div>
-        </>
     );
 }
